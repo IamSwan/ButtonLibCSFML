53b006b0515ef02c82ab1a4215f2cd60117b2463
Swan Sarli <swan.sarli@epitech.eu>
2023-04-12 15:51:55 +0200

    [ADD] added functions to Button.h

--- include/Button.h
+++ include/Button.h
@@ -14,3 +14,9 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include "ButtonStructs.h"
+
+sfSprite *create_button(sfTexture *texture, sfVector2f scale, sfVector2f pos);
+void button_destroy(sfSprite *sprite);
+void button_set_position(sfSprite *sprite, sfVector2f pos);
+void button_set_scale(sfSprite *sprite, sfVector2f scale);
+void button_set_texture(sfSprite *sprite, sfTexture *texture);
